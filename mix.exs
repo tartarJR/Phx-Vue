a724b5c4464764ae86e4f97b3281b7a23ae18ea6
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-07 15:22:44 +0200

    hound and whitebread dependencies added

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Rentit.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: ["white_bread.run": :test]
     ]
   end
 
@@ -42,7 +43,9 @@ defmodule Rentit.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:white_bread, "~> 4.3", only: [:test]},
+      {:hound, "~> 1.0"}
     ]
   end
 
