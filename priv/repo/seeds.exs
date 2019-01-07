afe500b613fc9ed4ee7fe024d37e39b0d7e9a628
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-07 23:07:07 +0200

    purchase seeder added

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,7 +10,7 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Rentit.{Repo, Plant}
+alias Rentit.{Repo, Plant, PurchaseOrder}
 
 [
   %{name: "Plant 1", price_per_day: 110.0, description: "Plant 1 description"},
@@ -20,3 +20,15 @@ alias Rentit.{Repo, Plant}
   %{name: "Plant 5", price_per_day: 100.0, description: "Plant 5 description"}
  ]
  |> Enum.each(fn plant -> Repo.insert!(Plant.changeset(%Plant{}, %{name: plant.name, price_per_day: plant.price_per_day, description: plant.description})) end)
+
+
+[
+  %{start_date: "2019-01-11", end_date: "2019-01-13", cost: 330, plant_id: 11 },
+  %{start_date: "2019-01-14", end_date: "2019-01-17", cost: 440, plant_id: 11 },
+  %{start_date: "2019-01-18", end_date: "2019-01-22", cost: 550, plant_id: 11 },
+  %{start_date: "2019-01-07", end_date: "2019-01-08", cost: 200, plant_id: 15 },
+  %{start_date: "2019-01-09", end_date: "2019-01-11", cost: 300, plant_id: 15 },
+  %{start_date: "2019-01-11", end_date: "2019-01-12", cost: 300, plant_id: 14 },
+  %{start_date: "2019-01-13", end_date: "2019-01-15", cost: 450, plant_id: 14 }
+]
+|> Enum.each(fn item -> Repo.insert!(PurchaseOrder.changeset(%PurchaseOrder{}, %{start_date: Date.from_iso8601!(item.start_date), end_date: Date.from_iso8601!(item.end_date), cost: item.cost, plant_id: item.plant_id  })) end)
