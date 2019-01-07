973d46a6519e81a4b345f84de95e2ff7646ae837
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-07 17:31:12 +0200

    app.js implemented

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,21 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import css from "../css/app.css"
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
-import "phoenix_html"
+import "axios";
+import plantSearch from "./plant-search";
+import orderConfirmation from "./order-confirmation";
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+Vue.use(VueRouter);
+
+Vue.component("plantSearch", plantSearch);
+Vue.component("orderConfirmation", orderConfirmation);
+
+var router = new VueRouter({
+  routes: [
+      { path: '/', component: plantSearch },
+      { path: '/plants/:plant_id', component: orderConfirmation, name: "select-plant" },
+      { path: '*', redirect: '/' }
+  ]
+});
+
+new Vue({router}).$mount("#app");
