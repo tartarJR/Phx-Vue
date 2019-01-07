1967a4653180e3a0a091509511df51b3af1b95c5
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-08 00:29:40 +0200

    order vue component added

---
+++ assets/js/order-confirmation.vue
@@ -0,0 +1,106 @@
+<template>
+    
+    <div>
+
+        <h3>Auction details</h3>
+        <div class="form-group">
+
+            <label class="control-label col-sm-3">Name: {{this.name}}</label>
+
+        </div>
+
+        <div class="form-group">
+
+            <label class="control-label col-sm-3">Description: {{this.description}}</label>
+
+        </div>
+
+        <div class="form-group">
+        
+            <label class="control-label col-sm-3">Start Date: {{this.startDate}}</label>
+
+        </div>
+
+        <div class="form-group">
+        
+            <label class="control-label col-sm-3">End Date: {{this.endDate}}</label>
+
+        </div>
+
+        <div class="form-group">
+        
+            <label class="control-label col-sm-3">Cost: {{this.pricePerDay}}</label>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+import axios from "axios";
+
+
+export default {
+    data: function() {
+      
+      return {
+        
+          plantId: 0,
+
+          name: "",
+
+          description: "",
+
+          startDate: "",
+
+          endDate: "",
+
+          pricePerDay: 0.0
+
+      }
+
+    },
+
+    mounted: function() {
+    
+        this.plantId = this.$route.params.plant_id
+
+        this.name = this.$route.params.name
+
+        this.description = this.$route.params.description
+
+        this.startDate = this.$route.params.start_date
+
+        this.endDate = this.$route.params.end_date
+
+        this.pricePerDay = this.$route.params.price_per_day
+
+        this.orderPlant()
+
+    },
+
+    methods: {
+      
+        orderPlant: function() {
+
+            axios.post("/api/order", {start_date: this.startDate, end_date: this.endDate, cost: this.pricePerDay, plant_id: this.plantId})
+                    .then(response => {
+                        this.response = response.data.msg
+                    }).catch(error => {
+                        console.log(error);
+                    });
+
+        }
+
+    },
+
+    computed: {
+      
+        
+
+    }
+
+}
+</script>
