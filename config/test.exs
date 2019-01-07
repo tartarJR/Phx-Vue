f7820b1317bb1bf19ede5815a6e8e348de0d7aae
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-07 18:04:47 +0200

    changed test server port

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :rentit, RentitWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 4001],
   server: true
 
 # Print only warnings and errors during test
