7e1d73c3dfb459829846621cd60e27fb254f4ff6
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-07 15:45:54 +0200

    plant seeding

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rentit.{Repo, Plant}
+
+[
+  %{name: "Plant 1", price_per_day: 110.0, description: "Plant 1 description"},
+  %{name: "Plant 2", price_per_day: 130.0, description: "Plant 2 description"},
+  %{name: "Plant 3", price_per_day: 120.0, description: "Plant 3 description"},
+  %{name: "Plant 4", price_per_day: 150.0, description: "Plant 4 description"},
+  %{name: "Plant 5", price_per_day: 100.0, description: "Plant 5 description"}
+ ]
+ |> Enum.each(fn plant -> Repo.insert!(Plant.changeset(%Plant{}, %{name: plant.name, price_per_day: plant.price_per_day, description: plant.description})) end)
