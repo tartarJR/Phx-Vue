725a372d46ab5ae0a486039a7ebe7c80dbddda26
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-07 17:32:56 +0200

    added vue components

---
+++ assets/js/plant-search.vue
@@ -0,0 +1,92 @@
+<template>
+    
+    <div>
+
+        <div class="form-group">
+
+            <label for="name">Name</label>
+
+            <input type="text" class="form-control" id="name" placeholder="Name" v-model="name">
+
+        </div>
+
+        <div class="form-group">
+
+            <label for="start-date">Start Date</label>
+
+            <input type="text" class="form-control" id="start-date" placeholder="Start Date" v-model="startDate">
+
+        </div>
+
+        <div class="form-group col-3">
+
+            <label for="end-date">End Date</label>
+
+            <input type="text" class="form-control" id="end-date" placeholder="End Date" v-model="endDate">
+            
+        </div>
+
+        <button type="submit" id="search-plant" class="btn btn-primary" v-on:click="searchPlants">Search Plants</button>
+        
+        <div id='list-wrapper' v-if="plants.length > 0">
+        
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="plant in plants" :key="plant.id">
+                        <td>{{ plant.name }}</td>
+                        <td>{{ plant.description }}</td>
+                        <td>{{ plant.price_per_day }}</td>
+                        <td>Select</td>
+                    </tr>
+                </tbody>
+            </table> 
+        
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+import axios from "axios";
+
+
+export default {
+    data: function() {
+      return {
+        
+        name: "",
+
+        startDate: "",
+
+        endDate: "",
+        
+        plants: []
+
+      }
+    },
+
+    mounted: function() {
+    
+    },
+
+    methods: {
+      
+        searchPlants: function() {
+
+
+
+        }
+
+    }
+}
+</script>
